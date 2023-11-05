4190068f2117c07f00d2819e765fbb5a8cf18995
Omar20_74 <omar.ragh37@gmail.com>
2023-11-05 23:43:26 +0200

    USART Final Version

--- MCAL/USART/USART_config.h
+++ MCAL/USART/USART_config.h
@@ -1,50 +1,19 @@
-/****************************************************************/
-/****************************************************************/
-/***********************   YEHIA SHAHIN   ***********************/
-/***********************   Layer :MCAL    ***********************/
-/***********************   SWC (DRIVER):USART *********************/
-/***********************   DATA : 1-9-2020 *********************/
-/***********************   VERSION : 1.00   *********************/
-/****************************************************************/
-/****************************************************************/
+/********************		Author: TAHER MOHAMED	**************/
+/********************		Layer: MCAL				**************/
+/********************		SWC: TIMER				**************/
+/********************		Version: 1.00			**************/
+/********************		Date: 28-8-2020			**************/
+/*****************************************************************/
+/*****************************************************************/
+#ifndef TIMER_CONFIG_H_
+#define TIMER_CONFIG_H_
+/*use or not use interrupt*/
+#define UART_WITH_INTERRUPT                  1
+#define UART_WITHOUT_INTERRUPT		         2
+#define INTERRUPT                   		 UART_WITH_INTERRUPT
 
-#ifndef USART_CONFIG_H_
-#define USART_CONFIG_H_
-
-#define USART_BAUD_RATE 			9600
-
-/*Select the size of data ,Options :
- *									1- SIZE_5BIT
- *									2- SIZE_6BIT
- *									3- SIZE_7BIT
- *									4- SIZE_8BIT
- *									5- SIZE_REV1
- *									6- SIZE_REV2
- *									7- SIZE_REV3
- *									8- SIZE_9BIT   */
-#define USART_SIZE_DATA				      SIZE_8BIT
-
-/*Select the usart mode ,Options : 1-ASY_MODE
- * 								   2-SYN_MODE   */
-#define USART_MODE					      ASY_MODE
-
-/*Select the Pariity Mode , Options :
- * 									1-PARITY_DIS
- * 									2-PARITY_REV
- * 									3-PARITY_EVEN
- * 									4-PARITY_ODD 		*/
-#define USART_PARITY_MODE 			      PARITY_DIS
-
-/*Select the the stop bit Mode , Options :1-STOP_1BIT
- 	 	 	 	 	 	 	 	 	 	  2-STOP_2BIT      */
-#define USART_STOP_BIT_MODE			      STOP_1BIT
-
-
-
-
-
-
-
+/*boud rate from table*/
+#define UART_BOUDRATE                       51
 
 
 
--- MCAL/USART/USART_program.c
+++ MCAL/USART/USART_program.c
@@ -1,390 +1,171 @@
-/****************************************************************/
-/****************************************************************/
-/***********************   YEHIA SHAHIN   ***********************/
-/***********************   Layer :MCAL    ***********************/
-/***********************   SWC (DRIVER):USART *******************/
-/***********************   DATA : 1-9-2020 **********************/
-/***********************   VERSION : 1.00   *********************/
-/****************************************************************/
-/****************************************************************/
-
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-#include <util/delay.h>
 
 #include "USART_register.h"
-#include "USART_.private.h"
-#include "USART_config.h"
 #include "USART_interface.h"
+#include "USART_config.h"
 
-#include "DIO_interface.h"
+/*Global pointer to function to hold receive ISR address*/
+void (*USART_pvReceiveFunc)(u8 copy_u8received)=NULL;
 
-
-static u8 State = ACTIVE;
-static u8 Local_u8Counter =0 ;
-static u8 USART_u8CounterREC ;
-static u8 USART_u8CounterLoop =1 ;
-static char * USART_charSendString = NULL ;
-static u8*  USART_u8DATA  ;
-void (*USART_CallBackFunc[2])(void)={NULL};
-
-void USART_voidUDRInit (u8 Copy_u8InitValue)
+void USART_voidInit(void)
 {
-	UDR = Copy_u8InitValue;
-}
-
-u8 USART_voidReadUDR (void)
-{
-	return UDR ;
-}
-
-u8 USART_voidEnable (u8 Copy_u8StateTX_RX)
-{
-	u8 Local_u8ErrorState = OK;
-	if (Copy_u8StateTX_RX == RX_DISABLE_TX_DISABLE)
+	if (INTERRUPT==UART_WITH_INTERRUPT)
 	{
-		CLR_BIT(UCSRB,UCSRB_RXEN);
-		CLR_BIT(UCSRB,UCSRB_TXEN);
-
-	}
-	else if (Copy_u8StateTX_RX== RX_ENABLE_TX_DISABLE)
-	{
-		SET_BIT(UCSRB,UCSRB_RXEN);
-		CLR_BIT(UCSRB,UCSRB_TXEN);
-	}
-	else if (Copy_u8StateTX_RX== RX_DISABLE_TX_ENABLE )
-	{
-		CLR_BIT(UCSRB,UCSRB_RXEN);
-		SET_BIT(UCSRB,UCSRB_TXEN);
-	}
-	else if (Copy_u8StateTX_RX==RX_ENABLE_TX_ENABLE  )
-	{
-		SET_BIT(UCSRB,UCSRB_RXEN);
-		SET_BIT(UCSRB,UCSRB_TXEN);
-	}
-	else
-	{
-		Local_u8ErrorState =NOK ;
-	}
-
-	return Local_u8ErrorState ;
-
-}
-
-u8 USART_u8ParityMode (u8 Copy_u8ParityMode)
-{
-	u8 Local_u8ErrorState =OK ;
-
-	if (Copy_u8ParityMode ==PARITY_DIS  )
-	{
-		CLR_BIT(UCSRC,UCSRC_UPM0);
-		CLR_BIT(UCSRC,UCSRC_UPM1);
-
-	}
-	else if (Copy_u8ParityMode  ==PARITY_DIS )
-	{
-		SET_BIT(UCSRC,UCSRC_UPM0);
-		CLR_BIT(UCSRC,UCSRC_UPM1);
-
-	}
-	else if (Copy_u8ParityMode ==PARITY_EVEN)
-	{
-		CLR_BIT(UCSRC,UCSRC_UPM0);
-		SET_BIT(UCSRC,UCSRC_UPM1);
-	}
-	else if (Copy_u8ParityMode  ==PARITY_ODD)
-	{
-		SET_BIT(UCSRC,UCSRC_UPM0);
-		SET_BIT(UCSRC,UCSRC_UPM1);
-	}
-	else
-	{
-		Local_u8ErrorState =NOK ;
-	}
-
-
-	return Local_u8ErrorState ;
-
-
-}
-
-u16 USART_u16UBRR (u16 Copy_u16BaudRate)
-{
-
-	u16 Local_u16UBRR ;
-
-	Local_u16UBRR =  (u16)((10000000)/(16UL * (u32)Copy_u16BaudRate)) -1UL ;
-
-	return Local_u16UBRR ;
-
-}
-
-
-void USART_voidInit (void)
-{
-	u8 Local_Helper =0  ;
-
-	SET_BIT(Local_Helper,UCSRC_URSEL);
-
-#if USART_SIZE_DATA < 5
-	Local_Helper |= ((USART_SIZE_DATA-1) << UCSRC_UCSZ0);
-	CLR_BIT(UCSRB,UCSRB_UCSZ2);
-
-	Local_Helper |= (USART_MODE<<UCSRC_UMSEL);
-	Local_Helper |= (USART_PARITY_MODE<<UCSRC_UPM0);
-	Local_Helper |= (USART_STOP_BIT_MODE<<UCSRC_USBS);
-
-
-
-#elif USART_SIZE_DATA >= 5
-	Local_Helper |= ((USART_SIZE_DATA-5) << UCSRC_UCSZ0);
-	SET_BIT(UCSRB,UCSRB_UCSZ2);
-
-	Local_Helper |= (USART_MODE<<UCSRC_UMSEL);
-	Local_Helper |= (USART_PARITY_MODE<<UCSRC_UPM0);
-	Local_Helper |= (USART_STOP_BIT_MODE<<UCSRC_USBS);
-
-
-#else
-#error "USART_SIZE_DATA is Wrong"
-#endif
-
-	UCSRC = Local_Helper ;
-
-	UBRRL = (u8)USART_u16UBRR(USART_BAUD_RATE) ;
-	UBRRH = (u8)((USART_u16UBRR(USART_BAUD_RATE))>>8);
-
-	USART_voidEnable (RX_ENABLE_TX_ENABLE);
-
-}
-
-u8 USART_u8SendData (u8 Copy_u8Data)
-{
-	u32 Local_u32Counter =0 ;
-	u8 Local_u8ErrorState = OK ;
-	while ((GET_BIT(UCSRA,UCSRA_UDRE) == 0) && (Local_u32Counter < TIME_OUT_TRANS))
-	{
-
-	}
-	if (Local_u32Counter < TIME_OUT_TRANS)
-	{
-		UDR = Copy_u8Data ;
-	}
-	else
-	{
-		Local_u8ErrorState =NOK ;
-	}
-	return Local_u8ErrorState ;
-
-}
-
-u8 USART_u8RecieveData (u8 * Copy_u8Data)
-{
-	u32 Local_u32Counter =0 ;
-	u8 Local_u8ErrorState = OK ;
-	if (Copy_u8Data != NULL)
-	{
-		while ((GET_BIT(UCSRA,UCSRA_RXC) == 0) &&( Local_u32Counter < TIME_OUT_REC))
-		{
-			Local_u32Counter++ ;
-		}
-		if (Local_u32Counter < TIME_OUT_REC)
-		{
-			while(1)
-			{
-			*Copy_u8Data = UDR ;
-			if (*Copy_u8Data < '0')
-			{
-
-			}
-			else
-			{
-				break;
-			}
-			}
-
-		}
-		else
-		{
-			Local_u8ErrorState =NOK ;
-		}
-	}
-	else
-	{
-		Local_u8ErrorState =NULL_POINTER ;
-	}
-	return Local_u8ErrorState ;
-
-}
-
-u8 USART_u8SendStringSyn (const char * Copy_pu8String)
-{
-	u8 Local_u8ErrorState =OK ;
-	int i ;
-
-	if (Copy_pu8String != NULL)
-	{
-		if (State == ACTIVE)
-		{
-			State =BUSY ;
-			for (i=0 ; Copy_pu8String[i] !=0 ;i++)
-			{
-				USART_u8SendData(Copy_pu8String[i]);
-			}
-
-
-		}
-		else
-		{
-			Local_u8ErrorState =NOK;
-		}
-		while (GET_BIT(UCSRA,UCSRA_UDRE)==0);
-		State =ACTIVE ;
-
-	}
-	else
-	{
-		Local_u8ErrorState =NULL_POINTER ;
-	}
-
-	return Local_u8ErrorState ;
-}
-
-u8 USART_u8ReceiveBufferSyn (u8 * Copy_pu8Data ,  u8 Copy_u8BufferSize)
-{
-	u8 Local_u32Counter =0  ;
-	u8 Local_u8ErrorState =OK ;
-	u8 i ;
-	if (Copy_pu8Data != NULL)
-	{
-
-		for (i=0 ; i<Copy_u8BufferSize ; i++ )
-		{
-			while ((GET_BIT(UCSRA,UCSRA_RXC) == 0) &&( Local_u32Counter < TIME_OUT_REC))
-			{
-
-			}
-			if ((Local_u32Counter < TIME_OUT_REC))
-			{
-
-				Copy_pu8Data[i] = UDR ;
-
-				if (Copy_pu8Data[i] < '0')
-				{
-					i--;
-				}
-
-			}
-			else
-			{
-				Local_u8ErrorState =NOK ;
-			}
-		}
-
-	}
-	else
-	{
-		Local_u8ErrorState = NULL_POINTER  ;
-	}
-
-
-	return Local_u8ErrorState ;
-}
-
-
-u8 USART_u8SendStringAsyn ( char *Copy_charString , void (*Copy_voidNotification)(void))
-{
-	u8 Local_u8ErrorState =OK ;
-	if (Copy_voidNotification !=NULL)
-	{
-		if (State == ACTIVE)
-		{
-			State =BUSY;
-		USART_charSendString = Copy_charString ;
-		USART_CallBackFunc[USART_UDE]= Copy_voidNotification ;
-		USART_u8SendData(USART_charSendString[Local_u8Counter]);
-		SET_BIT(UCSRB,UCSRB_UDRIE);
-		}
-
-	}
-	else
-	{
-		Local_u8ErrorState =NOK ;
-	}
-	return Local_u8ErrorState ;
-
-}
-u8 USART_u8RecieveAsyn (u8 * Copy_u8Data , u8 Copy_u8BufferSize , void (*Copy_voidNotifaction)(void) )
-{
-	u8 Local_u8ErrorState = OK;
-	if (Copy_voidNotifaction != NULL)
-	{
-		if (State == ACTIVE)
-		{
-			State =BUSY ;
-		USART_u8DATA = Copy_u8Data ;
-		USART_u8CounterREC = Copy_u8BufferSize ;
-		USART_CallBackFunc[USART_RXC] = Copy_voidNotifaction ;
+		u8 Local_u8UCSRCValue=0;
+		/*open interrupt to Receive Complete */
 		SET_BIT(UCSRB,UCSRB_RXCIE);
-		}
+		/*open interrupt to transmit Complete */
+		//SET_BIT(UCSRB,UCSRB_TXCIE);
+
+		//****** we cannot use get and set bit with ucsrc we will create variable help us ****//
+
+		//to use UCSRC
+		SET_BIT(Local_u8UCSRCValue,UCSRC_URSEL);
+
+		//set to 8 bit send and receive
+		CLR_BIT(UCSRB,UCSRB_UCSZ2);
+		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ1);
+		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ0);
+
+		/* mode select
+		 	 	 	      1-Synchronous
+		 	 	 	      2-ASynchronous */
+		//ASynchronous
+		CLR_BIT(Local_u8UCSRCValue,UCSRC_UMSEL);
+
+		// now will set this value in UCSRC at same time
+		UCSRC=Local_u8UCSRCValue;
+
+		//boudrate now 9600 with 8Mhz
+		UBRRL=UART_BOUDRATE;
+
+		//enable pins recieve & transmit
+		SET_BIT(UCSRB,UCSRB_TXEN);
+		SET_BIT(UCSRB,UCSRB_RXEN);
+
 	}
-	else
+	else if (INTERRUPT==UART_WITHOUT_INTERRUPT)
 	{
-		Local_u8ErrorState = NOK ;
+		/*we can not use set bit or clr bit here because it want in same time so we will creat a variable*/
+		u8 Local_u8UCSRCValue=0;
+		SET_BIT(Local_u8UCSRCValue,UCSRC_URSEL);  //to use UCSRC
+		//set to 8 bit send and receive
+		CLR_BIT(UCSRB,UCSRB_UCSZ2);
+		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ1);
+		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ0);
+		// now will set this value in UCSRC at same time
+		UCSRC=Local_u8UCSRCValue;
+		//boudrate now 9600 with 8Mhz
+		UBRRL=UART_BOUDRATE;
 
+		//enable
+		SET_BIT(UCSRB,UCSRB_TXEN);   //tx enable
+		SET_BIT(UCSRB,UCSRB_RXEN);   //rx enable
 	}
-
-	return Local_u8ErrorState ;
 }
 
-void __vector_14 (void)			__attribute__((signal));
-void __vector_14 (void)
+
+//function to receive data no 8bit
+u8 USART_u8Receive(void)
 {
-	if (USART_CallBackFunc[USART_UDE] != NULL)
+	//wait until be low by default it high when receive become low
+	while(GET_BIT(UCSRA,UCSRA_RXC)==0);
+	//return the receive data
+	return UDR;
+}
+
+void USART_voidSend(u8 Copy_u8data)
+{
+	//wait until transmit data empty
+	while(GET_BIT(UCSRA,UCSRA_UDRE)==0);
+	UDR=Copy_u8data;   //transmit data
+}
+
+void UART_voidSendSingleNumber(u8 Copy_u8number)
+{
+
+	switch(Copy_u8number)
 	{
-		Local_u8Counter++;
-		UDR= USART_charSendString[Local_u8Counter] ;
-		if (USART_charSendString [Local_u8Counter] == 0)
-		{
-			State =ACTIVE ;
-			Local_u8Counter =0 ;
-			CLR_BIT(UCSRB,UCSRB_UDRIE);
-			USART_CallBackFunc[USART_UDE]();
-		}
+	case 0 :USART_voidSend('0');break;
+	case 1 :USART_voidSend('1'); break;
+	case 2 :USART_voidSend('2') ; break;
+	case 3 :USART_voidSend('3') ; break;
+	case 4 :USART_voidSend('4') ; break;
+	case 5 :USART_voidSend('5') ; break;
+	case 6 :USART_voidSend('6') ; break;
+	case 7 :USART_voidSend('7') ; break;
+	case 8 :USART_voidSend('8') ; break;
+	case 9 :USART_voidSend('9') ; break;
 	}
-	else
+
+}
+void UART_voidSendNumber(u32 Copy_u8number)
+{
+	if(Copy_u8number < 10)
 	{
-		/*Nothing*/
+		UART_voidSendSingleNumber(Copy_u8number);
+
+	} else
+	{
+		UART_voidSendNumber(Copy_u8number / 10);
+		UART_voidSendSingleNumber(Copy_u8number % 10);
+
 	}
 }
 
-void __vector_13 (void)			__attribute__((signal));
+
+//send string
+void USART_voidSendString(u8 *str)
+{
+	u8 i;
+	for(i=0;i<100;i++) {
+		if((*(str+i))=='\0')
+		{break;}
+		USART_voidSend(*(str+i));
+		// UDR=*(str+i);
+	}
+}
+
+//get string
+void UARTGetString(u8 *str)  //Receive a character until carriage return or newline
+{
+	u8 i = 0;
+	str[i] = USART_u8Receive();
+	while(str[i] != '\n')
+	{
+		i++;
+		str[i] = USART_u8Receive();
+	}
+	str[i] = '\0';
+}
+
+/*part of interrupt*/
+
+void USART_voidRecieveSetCallBack(void (*Copy_pvRecieveFunc)(u8 copy_u8received))
+{
+	if(Copy_pvRecieveFunc != NULL )
+	{
+		USART_pvReceiveFunc=Copy_pvRecieveFunc;
+	}
+	else
+	{
+		/*Do nothing*/
+	}
+}
+
+
+void __vector_13 (void)  __attribute__((signal));
 void __vector_13 (void)
 {
-
-	if (USART_CallBackFunc[USART_RXC] != NULL)
+	if(USART_pvReceiveFunc != NULL)
 	{
-		USART_u8DATA[USART_u8CounterLoop-1] = UDR ;
-
-		if (USART_u8DATA[USART_u8CounterLoop-1] < '0')
-		{
-
-			USART_u8CounterLoop--;
-		}
-		USART_u8CounterLoop++;
-		DIO_u8SetPinValue(DIO_u8PORTA,DIO_u8PIN7,DIO_u8PIN_HIGH);
-		if (USART_u8CounterLoop==(USART_u8CounterREC+1))
-		{
-			USART_u8CounterLoop =1;
-			State =ACTIVE ;
-			CLR_BIT(UCSRB,UCSRB_RXCIE);
-			USART_CallBackFunc[USART_RXC]();
-		}
-
+		USART_pvReceiveFunc(UDR);
 	}
 	else
 	{
-		/*Nothing*/
+		/*Do nothing*/
 	}
-
 }
+
+
