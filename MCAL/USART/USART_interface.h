ac3b081034c3ea66cf1e95768aafac33433826eb
Omar20_74 <omar.ragh37@gmail.com>
2023-11-06 00:01:16 +0200

    added more comments to the code

--- MCAL/USART/USART_interface.h
+++ MCAL/USART/USART_interface.h
@@ -1,14 +1,28 @@
-
 #ifndef USART_INTERFACE_H_
 #define USART_INTERFACE_H_
 
-void USART_voidInit(void);               								//function to initial
-void USART_voidSend(u8 Copy_u8data);     								//function to transmit data
-u8 USART_u8Receive(void);                								//function to receive data
-void USART_voidRecieveSetCallBack(void (*Copy_pvRecieveFunc)(u8 copy_u8received));	//function set call back
+// Function to initialize the USART communication
+void USART_voidInit(void);
+
+// Function to send a single byte of data over USART
+void USART_voidSend(u8 Copy_u8data);
+
+// Function to receive a single byte of data from USART
+u8 USART_u8Receive(void);
+
+// Function to set a callback function for handling received data (used with interrupts)
+void USART_voidRecieveSetCallBack(void (*Copy_pvRecieveFunc)(u8 copy_u8received));
+
+// Function to send a string over USART
 void USART_voidSendString(u8 *str);
 
+// Function to receive a string until a newline character and store it in 'str'
 void UARTGetString(u8 *str);
+
+// Function to send a single-digit number (0-9) over USART
 void UART_voidSendSingleNumber(u8 Copy_u8number);
+
+// Function to send a multi-digit number (32-bit) over USART
 void UART_voidSendNumber(u32 Copy_u8number);
+
 #endif
--- MCAL/USART/USART_program.c
+++ MCAL/USART/USART_program.c
@@ -5,167 +5,162 @@
 #include "USART_interface.h"
 #include "USART_config.h"
 
-/*Global pointer to function to hold receive ISR address*/
-void (*USART_pvReceiveFunc)(u8 copy_u8received)=NULL;
+/* Global pointer to function to hold receive ISR address */
+void (*USART_pvReceiveFunc)(u8 copy_u8received) = NULL;
 
 void USART_voidInit(void)
 {
-	if (INTERRUPT==UART_WITH_INTERRUPT)
-	{
-		u8 Local_u8UCSRCValue=0;
-		/*open interrupt to Receive Complete */
-		SET_BIT(UCSRB,UCSRB_RXCIE);
-		/*open interrupt to transmit Complete */
-		//SET_BIT(UCSRB,UCSRB_TXCIE);
+    if (INTERRUPT == UART_WITH_INTERRUPT)
+    {
+        u8 Local_u8UCSRCValue = 0;
+        /* Enable interrupt for Receive Complete */
+        SET_BIT(UCSRB, UCSR_B_RXCIE);
+        /* Enable interrupt for transmit Complete (currently commented out) */
 
-		//****** we cannot use get and set bit with ucsrc we will create variable help us ****//
+        // We cannot use get and set bit with UCSRC, so we will create a variable for it.
 
-		//to use UCSRC
-		SET_BIT(Local_u8UCSRCValue,UCSRC_URSEL);
+        // To use UCSRC
+        SET_BIT(Local_u8UCSRCValue, UCSRC_URSEL);
 
-		//set to 8 bit send and receive
-		CLR_BIT(UCSRB,UCSRB_UCSZ2);
-		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ1);
-		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ0);
+        // Set to 8-bit data for both send and receive
+        CLR_BIT(UCSRB, UCSR_B_UCSZ2);
+        SET_BIT(Local_u8UCSRCValue, UCSRC_UCSZ1);
+        SET_BIT(Local_u8UCSRCValue, UCSRC_UCSZ0);
 
-		/* mode select
-		 	 	 	      1-Synchronous
-		 	 	 	      2-ASynchronous */
-		//ASynchronous
-		CLR_BIT(Local_u8UCSRCValue,UCSRC_UMSEL);
+        /* Mode select:
+           1 - Synchronous
+           2 - Asynchronous
+           Here, it's set to Asynchronous */
+        CLR_BIT(Local_u8UCSRCValue, UCSRC_UMSEL);
 
-		// now will set this value in UCSRC at same time
-		UCSRC=Local_u8UCSRCValue;
+        // Set the value in UCSRC
+        UCSRC = Local_u8UCSRCValue;
 
-		//boudrate now 9600 with 8Mhz
-		UBRRL=UART_BOUDRATE;
+        // Baud rate set to 9600 with a system clock of 8MHz
+        UBRRL = UART_BOUDRATE;
 
-		//enable pins recieve & transmit
-		SET_BIT(UCSRB,UCSRB_TXEN);
-		SET_BIT(UCSRB,UCSRB_RXEN);
-
-	}
-	else if (INTERRUPT==UART_WITHOUT_INTERRUPT)
-	{
-		/*we can not use set bit or clr bit here because it want in same time so we will creat a variable*/
-		u8 Local_u8UCSRCValue=0;
-		SET_BIT(Local_u8UCSRCValue,UCSRC_URSEL);  //to use UCSRC
-		//set to 8 bit send and receive
-		CLR_BIT(UCSRB,UCSRB_UCSZ2);
-		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ1);
-		SET_BIT(Local_u8UCSRCValue,UCSRC_UCSZ0);
-		// now will set this value in UCSRC at same time
-		UCSRC=Local_u8UCSRCValue;
-		//boudrate now 9600 with 8Mhz
-		UBRRL=UART_BOUDRATE;
-
-		//enable
-		SET_BIT(UCSRB,UCSRB_TXEN);   //tx enable
-		SET_BIT(UCSRB,UCSRB_RXEN);   //rx enable
-	}
+        // Enable pins for receive and transmit
+        SET_BIT(UCSRB, UCSR_B_TXEN);
+        SET_BIT(UCSRB, UCSR_B_RXEN);
+    }
+    else if (INTERRUPT == UART_WITHOUT_INTERRUPT)
+    {
+        // We cannot use set bit or clear bit here because it must happen at the same time, so we create a variable.
+        u8 Local_u8UCSRCValue = 0;
+        SET_BIT(Local_u8UCSRCValue, UCSRC_URSEL); // To use UCSRC
+        // Set to 8-bit data for both send and receive
+        CLR_BIT(UCSRB, UCSR_B_UCSZ2);
+        SET_BIT(Local_u8UCSRCValue, UCSRC_UCSZ1);
+        SET_BIT(Local_u8UCSRCValue, UCSRC_UCSZ0);
+        // Set the value in UCSRC
+        UCSRC = Local_u8UCSRCValue;
+        // Baud rate set to 9600 with a system clock of 8MHz
+        UBRRL = UART_BOUDRATE;
+        // Enable transmit and receive
+        SET_BIT(UCSRB, UCSR_B_TXEN); // Transmit enable
+        SET_BIT(UCSRB, UCSR_B_RXEN); // Receive enable
+    }
 }
 
-
-//function to receive data no 8bit
+// Function to receive data (8-bit)
 u8 USART_u8Receive(void)
 {
-	//wait until be low by default it high when receive become low
-	while(GET_BIT(UCSRA,UCSRA_RXC)==0);
-	//return the receive data
-	return UDR;
+    // Wait until the receive buffer is not empty (data is received)
+    while (GET_BIT(UCSRA, UCSRA_RXC) == 0);
+    // Return the received data
+    return UDR;
 }
 
 void USART_voidSend(u8 Copy_u8data)
 {
-	//wait until transmit data empty
-	while(GET_BIT(UCSRA,UCSRA_UDRE)==0);
-	UDR=Copy_u8data;   //transmit data
+    // Wait until the transmit buffer is empty (ready to send)
+    while (GET_BIT(UCSRA, UCSRA_UDRE) == 0);
+    UDR = Copy_u8data; // Transmit data
 }
 
 void UART_voidSendSingleNumber(u8 Copy_u8number)
 {
-
-	switch(Copy_u8number)
-	{
-	case 0 :USART_voidSend('0');break;
-	case 1 :USART_voidSend('1'); break;
-	case 2 :USART_voidSend('2') ; break;
-	case 3 :USART_voidSend('3') ; break;
-	case 4 :USART_voidSend('4') ; break;
-	case 5 :USART_voidSend('5') ; break;
-	case 6 :USART_voidSend('6') ; break;
-	case 7 :USART_voidSend('7') ; break;
-	case 8 :USART_voidSend('8') ; break;
-	case 9 :USART_voidSend('9') ; break;
-	}
-
+    // Send the corresponding character for the given number
+    switch (Copy_u8number)
+    {
+    case 0: USART_voidSend('0'); break;
+    case 1: USART_voidSend('1'); break;
+    case 2: USART_voidSend('2'); break;
+    case 3: USART_voidSend('3'); break;
+    case 4: USART_voidSend('4'); break;
+    case 5: USART_voidSend('5'); break;
+    case 6: USART_voidSend('6'); break;
+    case 7: USART_voidSend('7'); break;
+    case 8: USART_voidSend('8'); break;
+    case 9: USART_voidSend('9'); break;
+    }
 }
+
 void UART_voidSendNumber(u32 Copy_u8number)
 {
-	if(Copy_u8number < 10)
-	{
-		UART_voidSendSingleNumber(Copy_u8number);
-
-	} else
-	{
-		UART_voidSendNumber(Copy_u8number / 10);
-		UART_voidSendSingleNumber(Copy_u8number % 10);
-
-	}
+    if (Copy_u8number < 10)
+    {
+        UART_voidSendSingleNumber(Copy_u8number);
+    }
+    else
+    {
+        UART_voidSendNumber(Copy_u8number / 10);
+        UART_voidSendSingleNumber(Copy_u8number % 10);
+    }
 }
 
-
-//send string
+// Send a string
 void USART_voidSendString(u8 *str)
 {
-	u8 i;
-	for(i=0;i<100;i++) {
-		if((*(str+i))=='\0')
-		{break;}
-		USART_voidSend(*(str+i));
-		// UDR=*(str+i);
-	}
+    u8 i;
+    for (i = 0; i < 100; i++)
+    {
+        if (*(str + i) == '\0')
+        {
+            break;
+        }
+        USART_voidSend(*(str + i));
+    }
 }
 
-//get string
-void UARTGetString(u8 *str)  //Receive a character until carriage return or newline
+// Get a string
+void UARTGetString(u8 *str)
 {
-	u8 i = 0;
-	str[i] = USART_u8Receive();
-	while(str[i] != '\n')
-	{
-		i++;
-		str[i] = USART_u8Receive();
-	}
-	str[i] = '\0';
+    u8 i = 0;
+    str[i] = USART_u8Receive();
+    while (str[i] != '\n')
+    {
+        i++;
+        str[i] = USART_u8Receive();
+    }
+    str[i] = '\0';
 }
 
-/*part of interrupt*/
+/* Part of interrupt */
 
 void USART_voidRecieveSetCallBack(void (*Copy_pvRecieveFunc)(u8 copy_u8received))
 {
-	if(Copy_pvRecieveFunc != NULL )
-	{
-		USART_pvReceiveFunc=Copy_pvRecieveFunc;
-	}
-	else
-	{
-		/*Do nothing*/
-	}
+    if (Copy_pvRecieveFunc != NULL)
+    {
+        USART_pvReceiveFunc = Copy_pvRecieveFunc;
+    }
+    else
+    {
+        /* Do nothing */
+    }
 }
 
-
-void __vector_13 (void)  __attribute__((signal));
-void __vector_13 (void)
+// USART Receive Interrupt Service Routine
+void __vector_13(void) __attribute__((signal));
+void __vector_13(void)
 {
-	if(USART_pvReceiveFunc != NULL)
-	{
-		USART_pvReceiveFunc(UDR);
-	}
-	else
-	{
-		/*Do nothing*/
-	}
+    if (USART_pvReceiveFunc != NULL)
+    {
+        // Call the callback function and pass the received data
+        USART_pvReceiveFunc(UDR);
+    }
+    else
+    {
+        /* Do nothing */
+    }
 }
-
-
